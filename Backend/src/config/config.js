6e6e849cd0b53bb7d96a943723297a369ee2bd5a
DoTung04 <167160103+DoTung04@users.noreply.github.com>
2025-10-30 17:50:23 +0700

    Update config.js

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -22,6 +22,9 @@ module.exports = {
   },
   email: {
     provider: process.env.EMAIL_PROVIDER || 'smtp',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+    from: process.env.EMAIL_FROM,
     smtp: {
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
